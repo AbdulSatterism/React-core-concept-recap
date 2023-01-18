319b52fde4df0748d800717e6da7ceb12247445d
Md. Abdul Satter <116937243+AbdulSatterism@users.noreply.github.com>
2023-01-18 18:25:52 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <District name='Pori appha'></District> */}
+
+      <LoadPosts></LoadPosts>
     </div>
   );
 }
 
+function LoadPosts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setPosts(data))
+  }, [])
+  return (
+    <div>
+      <h1>Posts :{posts.length}</h1>
+      {
+        posts.map(post => <Post post={post} key={post.id}></Post>)
+      }
+    </div>
+  )
+}
+
+function Post(props) {
+
+  const { title, body } = props.post;
+  return (
+    <div style={{ backgroundColor: 'lightcyan', border: '1px solid gray', margin: '10px' }}>
+      <h2>Title : {title}</h2>
+      <p>body : {body}</p>
+    </div>
+  )
+}
+
+/*
+function District(props) {
+  const [count, setCount] = useState(0);
+  const isIncrease = () => {
+    const newCount = count + 1;
+    setCount(newCount)
+  };
+  const isDecrease = () => setCount(count - 1);
+  return (
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={isIncrease}>increase</button>
+      <button onClick={isDecrease}>decrease</button>
+      <h2>Name :{props.name} </h2>
+    </div>
+  )
+}
+*/
 export default App;
